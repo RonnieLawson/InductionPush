ca0b96b6961deb8b35d9a182464f46ae6493729a
RonnieLawson <Ronnie.Lawson@esendex.com>
2017-03-05 14:42:16 +0000

    whoops

---
+++ InductionPush/Controllers/InboundMessage.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace InductionPush.Controllers
+{
+    public class InboundMessage
+    {
+        public Guid Id { get; set; }
+        public Guid MessageId { get; set; }
+        public Guid AccountId { get; set; }
+        public string MessageText { get; set; }
+        public string From { get; set; }
+        public string To { get; set; }
+    }
+}
