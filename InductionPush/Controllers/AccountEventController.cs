432d8b82939dc11f409b5fc298eae22da40328c7
RonnieLawson <Ronnie.Lawson@esendex.com>
2017-03-06 16:19:26 +0000

    Added accountEvent handler

---
+++ InductionPush/Controllers/AccountEventController.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Http;
+using InductionPush.Models;
+
+namespace InductionPush.Controllers
+{
+    public class AccountEventController : ApiController
+    {
+        private readonly EmailSender _emailSender = new EmailSender();
+        
+        public async void Post()
+        {
+            var raw = await RawContentReader.Read(Request);
+            System.Diagnostics.Trace.TraceInformation(raw);
+
+            /*if (inboundMessage == null)
+            {
+                System.Diagnostics.Trace.TraceError("Null Inbound Message");
+                return;
+            }*/
+
+            /*System.Diagnostics.Trace.TraceInformation($"Message Received from {inboundMessage.From}");
+            System.Diagnostics.Trace.TraceInformation($"Message Text: {inboundMessage.MessageText}");
+            
+            _emailSender.SendEmail($"Message Received from {inboundMessage.From}", inboundMessage.MessageText);           */
+        }
+    }
+}
