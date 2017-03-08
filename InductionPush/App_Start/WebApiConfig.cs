dbaacd0ef6b4c3a7bd44c72402f6677fd0ec8012
RonnieLawson <Ronnie.Lawson@esendex.com>
2017-03-08 08:50:32 +0000

    added account event handlers

--- InductionPush/App_Start/WebApiConfig.cs
+++ InductionPush/App_Start/WebApiConfig.cs
@@ -9,7 +9,6 @@ namespace InductionPush
     {
         public static void Register(HttpConfiguration config)
         {
-            // Web API configuration and services
 
             // Web API routes
             config.MapHttpAttributeRoutes();
@@ -17,8 +16,45 @@ namespace InductionPush
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
+                defaults: new {id = RouteParameter.Optional}
             );
+
+            /*config.Routes.MapHttpRoute(
+                name: "MessageReceived",
+                routeTemplate: "api/{controller}/{id}/{originator}",
+                defaults: new
+                {
+                    notificationType = RouteParameter.Optional,
+                    id = RouteParameter.Optional,
+                    originator = RouteParameter.Optional,
+                    recipient = RouteParameter.Optional,
+                    body = RouteParameter.Optional,
+                    type = RouteParameter.Optional,
+                    receivedAt = RouteParameter.Optional,
+                }
+            );
+            config.Routes.MapHttpRoute(
+                name: "MessageEvent",
+                routeTemplate: "api/{controller}/{id}/{eventType}/{occurredat}",
+                defaults: new
+                {
+                    notificationType = RouteParameter.Optional,
+                    id = RouteParameter.Optional,
+                    eventType = RouteParameter.Optional,
+                    occurredAt = RouteParameter.Optional,
+                    }
+            );
+            config.Routes.MapHttpRoute(
+                name: "MessageError",
+                routeTemplate: "api/{controller}/{id}/{eventType}/{occurredat}",
+                defaults: new
+                {
+                    notificationType = RouteParameter.Optional,
+                    id = RouteParameter.Optional,
+                    errorType = RouteParameter.Optional,
+                    detail = RouteParameter.Optional
+                }
+            );*/
         }
     }
 }
--- InductionPush/Controllers/AccountEventController.cs
+++ InductionPush/Controllers/AccountEventController.cs
@@ -1,28 +1,50 @@
 ﻿using System.Web.Http;
-using InductionPush.Models;
 
 namespace InductionPush.Controllers
 {
     public class AccountEventController : ApiController
     {
         private readonly EmailSender _emailSender = new EmailSender();
-        
-        public async void Post(MessageDelivered messageDelivered)
+
+        [HttpPost]
+        public async void Post()
+
         {
-            System.Diagnostics.Trace.TraceInformation($"AccountEvent Received");
+            System.Diagnostics.Trace.TraceInformation($"Unhandled Event Received");
+            var raw = await RawContentReader.Read(Request);
+            System.Diagnostics.Trace.TraceInformation(raw);
+        }
+
+        public async void MessageReceived(string notificationType, string id, string originator, string recipient, string body, string type, string receivedAt)
+        {
+            var raw = await RawContentReader.Read(Request);
+            System.Diagnostics.Trace.TraceInformation(raw);
+            System.Diagnostics.Trace.TraceInformation($"Message Received from {originator}");
+            System.Diagnostics.Trace.TraceInformation($"Sending email");
+
+            _emailSender.SendEmail($"Message Received", $"Message Received {receivedAt}: {body}");
+        }
+
+        public async void MessageEvent(string notificationType, string id, string eventType, string occurredAt)
+        {
             var raw = await RawContentReader.Read(Request);
             System.Diagnostics.Trace.TraceInformation(raw);
 
-            /*if (messageDelivered == null)
-            {
-                System.Diagnostics.Trace.TraceError("Null Message Delivered");
-                return;
-            }
-
-            System.Diagnostics.Trace.TraceInformation($"Message Delivered from {messageDelivered.Id}");
+            System.Diagnostics.Trace.TraceInformation($"Message Event: {eventType}");
             System.Diagnostics.Trace.TraceInformation($"Sending email");
 
-            _emailSender.SendEmail($"Message Delivered", $"Message Delivered {messageDelivered.OccurredAt.ToShortDateString()} - {messageDelivered.OccurredAt.ToShortTimeString()}");           */
+            _emailSender.SendEmail($"Message Event", $"Message Event Received {eventType} at {occurredAt}");
+        }
+
+        public async void MessageError(string notificationType, string id, string errorType, string occurredAt, string detail)
+        {
+            var raw = await RawContentReader.Read(Request);
+            System.Diagnostics.Trace.TraceInformation(raw);
+
+            System.Diagnostics.Trace.TraceInformation($"Message Error: {errorType}");
+            System.Diagnostics.Trace.TraceInformation($"Sending email");
+
+            _emailSender.SendEmail($"Message Error", $"Message Error Received {errorType} at {occurredAt}");
         }
     }
 }
