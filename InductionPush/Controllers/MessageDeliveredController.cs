74c19caa25a6036164cd05712b4dc8f1edfd8c14
RonnieLawson <Ronnie.Lawson@esendex.com>
2017-04-18 08:43:26 +0100

    Added MEddage Delivery

---
+++ InductionPush/Controllers/MessageDeliveredController.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Http;
+using InductionPush.Models;
+
+namespace InductionPush.Controllers
+{
+    public class MessageDeliveredController : ApiController
+    {
+        private readonly EmailSender _emailSender = new EmailSender();
+        
+        public async void Post(MessageDelivered deliveredMessage)
+        {
+            var raw = await RawContentReader.Read(Request);
+            System.Diagnostics.Trace.TraceInformation(raw);
+
+            if (deliveredMessage == null)
+            {
+                System.Diagnostics.Trace.TraceError("Null Inbound Message");
+                return;
+            }
+
+            System.Diagnostics.Trace.TraceInformation($"Message {deliveredMessage.Id} was been delivered at {deliveredMessage.OccurredAt.ToShortDateString()} : {deliveredMessage.OccurredAt.ToShortTimeString()}");
+
+            _emailSender.SendEmail($"Message Delivered", $"Message {deliveredMessage.Id} was been delivered at {deliveredMessage.OccurredAt.ToShortDateString()} : {deliveredMessage.OccurredAt.ToShortTimeString()}");
+        }
+    }
+}
